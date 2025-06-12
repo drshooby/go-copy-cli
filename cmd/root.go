e7a97bc875187b515b4675a0fa0a671ae9bd3f76
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-06-11 19:04:14 -0700

    add(opts): update scaffolding

--- cmd/root.go
+++ cmd/root.go
@@ -2,10 +2,17 @@ package copy
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+type Options struct {
+	Recursive      bool
+	Preserve       bool
+	FollowSymlinks bool
+}
 
 var (
 	rootCmd = &cobra.Command{
@@ -15,21 +22,40 @@ var (
 		Args: func(cmd *cobra.Command, args []string) error {
 			validator := cobra.MatchAll(
 				cobra.OnlyValidArgs,
-				cobra.MinimumNArgs(1),
+				cobra.MinimumNArgs(2),
 			)
 			return validator(cmd, args)
 		},
 
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hi")
+			fmt.Println(args)
 		},
 	}
 
-	Recursive bool
+	opts = &Options{}
 )
 
+func Copy(paths []string, dest string) error {
+	for _, src := range paths {
+		info, err := os.Lstat(src)
+		if err != nil {
+			log.Printf("Warning: cannot access %s: %v", src, err)
+			continue
+		}
+		if info.IsDir() {
+			if !opts.Recursive {
+				return fmt.Errorf("%s is a directory (use -r to copy directories)", src)
+			}
+			// copy directory
+		} else {
+			// copy file or symlink
+		}
+	}
+	return nil
+}
+
 func init() {
-	rootCmd.PersistentFlags().BoolVarP(&Recursive, "recursive", "r", false, "copy directories recursively")
+	rootCmd.PersistentFlags().BoolVarP(&opts.Recursive, "recursive", "r", false, "copy directories recursively")
 }
 
 func Execute() {
